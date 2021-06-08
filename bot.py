320ee3e1643a8660f577e217d6a01cd6248efad9
David-raj123 <64373160+David-raj123@users.noreply.github.com>
2021-06-08 11:20:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 from os import environ
 import aiohttp
-from pyrogram import Client, Filters
-InlineKeyboardButton, InlineKeyboardMarkup,
+
+from pyrogram import Client, filters
+from pyrogram import InlineKeyboardButton, InlineKeyboardMarkup
 
 API_ID = environ.get('API_ID')
 API_HASH = environ.get('API_HASH')
